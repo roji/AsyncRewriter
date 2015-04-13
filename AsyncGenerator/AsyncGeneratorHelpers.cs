99271f8e7f115afc236e729625c63266ef947a49
Shay Rojansky <roji@roji.org>
2015-04-13 09:48:00 +0100

    Nugetification

---
+++ AsyncGenerator/AsyncGeneratorHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This file was injected into your project by the AsyncGenerator package
+
+namespace AsyncGenerator
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    internal class GenerateAsyncAttribute : Attribute
+    {
+        public GenerateAsyncAttribute(string transformedName = null, bool withOverride = false) { }
+    }
+}
