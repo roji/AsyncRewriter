d04374709d358298e6ffc46cc8db62c34bbc73ec
Shay Rojansky <roji@roji.org>
2015-08-14 12:54:21 +0200

    Add playground test
    Which rewrites the Npgsql sources, to help debug issues.

---
+++ Tests/Playground.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsyncRewriter;
+using NUnit.Framework;
+
+namespace Tests
+{
+    class Playground
+    {
+        [Test, Explicit]
+        public void Npgsql()
+        {
+            const string npgsqlPath = @"C:\projects\npgsql\src\Npgsql";
+
+            var rewriter = new Rewriter(new ConsoleLoggingAdapter(LogLevel.Debug));
+            var generatedCode = rewriter.RewriteAndMerge(Directory.EnumerateFiles(npgsqlPath, "*.cs", SearchOption.AllDirectories).ToArray());
+            Console.WriteLine(generatedCode);
+        }
+    }
+}
