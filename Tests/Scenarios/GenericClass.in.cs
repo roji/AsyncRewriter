96a3abb7d28b1ea13ed020fbb925f8b45ede2006
Shay Rojansky <roji@roji.org>
2015-08-17 02:32:26 +0200

    Bumped version to 0.6.5

---
+++ Tests/Scenarios/GenericClass.in.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AsyncRewriter;
+
+namespace Tests.Scenarios
+{
+    class Simple<T>
+    {
+        [RewriteAsync]
+        public int Foo()
+        {
+            return 8;
+        }
+    }
+}
---
+++ Tests/Scenarios/GenericClass.out.cs
@@ -0,0 +1,18 @@
+﻿#pragma warning disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tests.Scenarios
+{
+    class Simple<T>
+    {
+        public async Task<int> FooAsync(CancellationToken cancellationToken)
+        {
+            return 8;
+        }
+    }
+}
