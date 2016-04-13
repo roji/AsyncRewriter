49197f8b8e3588ab1169f6370fe9f7ee87addf31
Shay Rojansky <roji@roji.org>
2016-04-13 17:39:18 +0300

    Port to dotnet rc2
    * Target netstandard13
    * Created a dotnet cli-compatible tool
    * Bump version to 0.8

--- src/dotnet-rewrite-async/Program.cs
+++ src/dotnet-rewrite-async/Program.cs
@@ -1,37 +1,33 @@
-﻿#if DNX451 || DOTNET5_4
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.PlatformAbstractions;
 using Microsoft.Extensions.FileSystemGlobbing;
+using AsyncRewriter;
 
 namespace AsyncRewriter.Commands
 {
     public class Program
     {
-        readonly string _projectDir;
         const string OutputFileName = "GeneratedAsync.cs";
 
-        public Program()
-        {
-            // TODO: Is there some way to extract the actual file list from the DNX runtime?
-            // This way we'd take excludes/includes, external directories, etc. into account
-            _projectDir = PlatformServices.Default.Application.ApplicationBasePath;
-        }
-
-        public void Main(string[] args)
+        public static int Main(string[] args)
         {
             var matcher = new Matcher();
             matcher.AddInclude(@"**\*.cs");
             matcher.AddExclude(OutputFileName);
 
+            var inputFiles = matcher.GetResultsInFullPath(".").ToArray();
+            Console.WriteLine("Rewriting async methods");
+            var asyncCode = new Rewriter().RewriteAndMerge(inputFiles);
+            File.WriteAllText(OutputFileName, asyncCode);
+                /*
             var inputFiles = new List<string>();
             var latestChange = DateTime.MinValue;
-            foreach (var f in matcher.GetResultsInFullPath(_projectDir))
+            foreach (var f in matcher.GetResultsInFullPath("."))
             {
                 inputFiles.Add(f);
                 var change = File.GetLastWriteTimeUtc(f);
@@ -39,18 +35,18 @@ namespace AsyncRewriter.Commands
                     latestChange = change;
             }
 
-            var outputFile = Path.Combine(_projectDir, OutputFileName);
-            if (!File.Exists(outputFile) || latestChange > File.GetLastWriteTimeUtc(outputFile))
+            if (!File.Exists(OutputFileName) || latestChange > File.GetLastWriteTimeUtc(OutputFileName))
             {
-                Console.WriteLine("Rewriting async methods in " + _projectDir);
+                Console.WriteLine("Rewriting async methods");
                 var asyncCode = new Rewriter().RewriteAndMerge(inputFiles.ToArray());
-                File.WriteAllText(outputFile, asyncCode);
+                File.WriteAllText(OutputFileName, asyncCode);
             }
             else
             {
                 Console.WriteLine("Skipping async rewriting, generated code up to date");
             }
+            */
+            return 0;
         }
     }
 }
-#endif
